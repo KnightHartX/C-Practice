540de21687ccd2b5949afdacbbacc7440e5d86a7
knighthart <450135440@qq.com>
2018-07-28 17:49:32 +0800

    add:2-6_sourcecode

---
+++ chapter2/2-6convert.cpp
@@ -0,0 +1,17 @@
+//程序清单2-6展示的是一个进制转换程序，主要涉及到的还是方法的调用
+#include <iostream>
+using namespace std;
+float stonetopounds(float n)
+{
+    float pounds=n*15;
+    return pounds;
+}
+int main()
+{
+    cout<<"input stones:";
+    float stones;
+    cin>>stones;
+    float pounds=stonetopounds(stones);
+    cout<<stones<<" stones = "<<pounds<<"pounds"<<endl;
+    return 0;
+}
