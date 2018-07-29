8466d04f984ca59d23f7e3cd002ce7b6fd34a415
knighthart <450135440@qq.com>
2018-07-29 20:02:49 +0800

    add:3-2source

--- chapter3/3-2exceed.cpp
+++ chapter3/3-2exceed.cpp
@@ -1,4 +1,5 @@
 //程序3-2主要比较了有符号数和无符号数的范围以及越界情况。
+//注意，C++程序中不要出现中文！
 #include <iostream>
 #include <climits>
 using namespace std;
@@ -6,9 +7,9 @@ int main()
 {
     int signed_n_max = INT_MAX;
     unsigned int unsigned_n_max = signed_n_max;
-    cout << "最大有符号整数+1的结果为：" << signed_n_max + 1 << endl;
-    cout << "和最大有符号整数大小相同的无符号整数+1结果为：" << unsigned_n_max + 1 << endl;
+    cout << "the maxvalue of signed inter+1:" << signed_n_max + 1 << endl;//最大有符号整数+1的结果
+    cout << "the same value of INT_MAX of unsigned+1:" << unsigned_n_max + 1 << endl;//和最大有符号整数大小相同的无符号整数+1结果
     signed_n_max = unsigned_n_max = 0;
-    cout << "有符号0-1结果为：" << signed_n_max - 1 << endl;
-    cout << "无符号0-1结果为：" << unsigned_n_max - 1 << endl;
+    cout << "signed 0 -1:" << signed_n_max - 1 << endl;//有符号0-1结果
+    cout << "unsigned 0 -1:" << unsigned_n_max - 1 << endl;//无符号0-1结果为：
 }
