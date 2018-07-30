caee7e71ac391e6aca7512867ae0bd5adf964422
knighthart <450135440@qq.com>
2018-07-30 08:36:15 +0800

    add:3-4source

--- chapter3/3-4hexoct2.cpp
+++ chapter3/3-4hexoct2.cpp
@@ -6,7 +6,9 @@ int main()
     int a = 1000;   //十进制
     int b = 0x0010; //十六进制
     int c = 042;    //八进制
-    cout << a << endl
-         << b << endl
-         << c << endl; //a、b、c输出的为十进制格式
+    cout << a << endl;//以十进制格式输出a
+    cout<<hex;//更改cout的输出格式为16进制
+    cout<< b << endl;//以十六进制的格式输出b
+    cout<<oct;//更改cout的输出格式为8进制
+    cout<< c << endl;//以八进制格式输出c
 }
